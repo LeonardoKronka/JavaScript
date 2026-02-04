f0befd7907a09de0698df8c8c8f559b81356c700
LeonardoKronka <leonardo.k.rodrigues@aluno.senai.br>
2026-02-04 14:29:27 -0300

    Exemplos e exercicios

---
+++ Operadores e Funções/funcoes.js
@@ -0,0 +1,52 @@
+//__________
+console.log("-----------------------------------")
+console.log("FUNÇÕES EM JAVASCRIPT");
+
+function saudacao() {
+    console.log("Olá, JavaScript!");
+}
+
+saudacao();
+
+//_____________
+
+function nome(nome) {
+    console.log("Olá, " + nome);
+}
+
+nome("Leo")
+
+function somar(a,b) {
+    return a + b;
+}
+
+console.log(somar(5,3));
+
+
+
+//EXERCICIOS
+console.log("EXERCICIOS FUNÇÕES")
+//7. Criar função que calcula o IMC = peso/(alt*alt)
+console.log("IMC")
+let peso = 70
+let altura = 1.80
+
+function IMC(peso, altura) {
+    return peso/(altura*altura);
+}
+console.log("O imc de uma pessoa com " + peso + "kg e " + altura + " de altura é: " + IMC(peso,altura));
+
+//8. Criar função que verifica se o número é par ou ímpar
+console.log("PAR E IMPAR")
+
+let num = 7;
+
+function parImpar(num) {
+    if (num % 2 === 0) {
+        return "Par";
+    } else {
+        return "Ímpar";
+    }
+}
+
+console.log("O número " + num + " é: " + parImpar(num));
